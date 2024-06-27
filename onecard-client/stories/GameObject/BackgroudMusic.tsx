c6455af80c2630443a498fe9015afeb19e936fa1
dong young <dong5854@gmail.com>
2024-06-27 22:06:22 +0900

    feat: add eventlisteners to play bgm

--- onecard-client/stories/GameObject/BackgroudMusic.tsx
+++ onecard-client/stories/GameObject/BackgroudMusic.tsx
@@ -9,18 +9,19 @@ interface BackgroundMusicProps {
 export const BackgroundMusic = ({url}: BackgroundMusicProps) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(true);
+  const [isUserInteracted, setIsUserInteracted] = useState(false);
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play().catch((error) => {
+        console.error("Failed to play audio:", error);
+      });
+    }
+  };
 
   useEffect(() => {
     audioRef.current = new Audio(url);
     audioRef.current.loop = true;
-
-    const playAudio = () => {
-      if (audioRef.current) {
-        audioRef.current.play().catch((error) => {
-          console.error("Failed to play audio:", error);
-        });
-      }
-    };
 
     playAudio();
 
@@ -30,8 +31,26 @@ export const BackgroundMusic = ({url}: BackgroundMusicProps) => {
         audioRef.current = null;
       }
     };
-
   }, [url]);
+
+  useEffect(() => {
+    const handleUserInteraction = () => {
+      if (!isUserInteracted) {
+        setIsUserInteracted(true);
+        playAudio();
+      }
+    };
+
+    document.addEventListener('click', handleUserInteraction);
+    document.addEventListener('touchstart', handleUserInteraction);
+    document.addEventListener('keydown', handleUserInteraction);
+
+    return () => {
+      document.removeEventListener('click', handleUserInteraction);
+      document.removeEventListener('touchstart', handleUserInteraction);
+      document.removeEventListener('keydown', handleUserInteraction);
+    };
+  }, [isUserInteracted]);
 
   const togglePlayPause = () => {
     if (audioRef.current) {
@@ -49,10 +68,10 @@ export const BackgroundMusic = ({url}: BackgroundMusicProps) => {
   return (
       <div className="button-container">
         <button
-            className={`pixel-button ${isPlaying ? 'off' : 'on'}`}
+            className={`mini-pixel-button ${isPlaying ? 'on' : 'off'}`}
             onClick={togglePlayPause}
         >
-          {isPlaying ? <FaVolumeMute size={24}/> : <FaVolumeUp size={24}/>}
+          {isPlaying ? <FaVolumeUp style={{ width: '60%', height: '60%' }} /> : <FaVolumeMute style={{ width: '60%', height: '60%' }} />}
         </button>
       </div>
   );
