7994f9ebb6e4de1876a3b0f05e99a0d227bed45f
dong young <dong5854@gmail.com>
2024-06-28 21:47:36 +0900

    feat: add game url

---
+++ onecard-client/app/game/single-player/page.tsx
@@ -0,0 +1,8 @@
+import GameTitle from "@/stories/UI/GameTitle";
+
+export default function SinglePlayerPage() {
+    return (
+        <div className="grid grid-cols-3 grid-rows-3 min-h-full">
+        </div>
+    );
+}
--- onecard-client/app/page.tsx
+++ onecard-client/app/page.tsx
@@ -1,17 +1,20 @@
+import Link from "next/link"
 import GameTitle from "@/stories/UI/GameTitle";
 import PixelRetroButton from "@/stories/UI/PixelRetroButton";
 
 export default function MainPage() {
     return (
-        <>
+        <div className="flex flex-col justify-center items-center mb-40">
             <GameTitle
                 title="ONE CARD"
                 subtitle="WEB GAME"
             />
-            <PixelRetroButton
-                text="START GAME"
-                className="mt-6"
-            />
-        </>
+            <Link href="/game/single-player">
+                <PixelRetroButton
+                    text="START GAME"
+                    className="mt-6"
+                />
+            </Link>
+        </div>
     );
 }
