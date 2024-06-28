b14717bebade10b6af2e438aa3b793a5ae748bfb
dong young <dong5854@gmail.com>
2024-06-28 20:10:27 +0900

    feat: set margin-top to start game button

--- onecard-client/app/page.tsx
+++ onecard-client/app/page.tsx
@@ -18,6 +18,7 @@ export default function MainPage() {
                 />
                 <PixelRetroButton
                     text="START GAME"
+                    className="mt-6"
                 />
             </div>
         </main>
--- onecard-client/stories/UI/PixelRetroButton.tsx
+++ onecard-client/stories/UI/PixelRetroButton.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import './PixelRetroButton.css';
+import {className} from "postcss-selector-parser";
 
 interface PixelRetroButtonProps {
   text: string;
+  className: string;
   onClick?: () => void;
 }
 
-const PixelRetroButton = ({ text, onClick }: PixelRetroButtonProps) => {
+const PixelRetroButton = ({ text, className, onClick }: PixelRetroButtonProps) => {
   const [isPressed, setIsPressed] = useState(false);
   const buttonClass = isPressed ? 'pixel-button pressed' : 'pixel-button';
 
   return (
       <button
-          className={buttonClass}
+          className={`${buttonClass} ${className}`}
           onMouseDown={() => setIsPressed(true)}
           onMouseUp={() => setIsPressed(false)}
           onMouseLeave={() => setIsPressed(false)}
