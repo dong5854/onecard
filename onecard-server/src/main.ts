95d0a2159657f1fe7e20079fbc3141b01df3383c
dong young <dong5854@gmail.com>
2025-11-14 03:27:00 +0900

    feat(server): setup swagger docs

--- onecard-server/src/main.ts
+++ onecard-server/src/main.ts
@@ -1,9 +1,23 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Onecard API')
+    .setDescription('REST API specification for the Onecard service')
+    .setVersion('1.0.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
